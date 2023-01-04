f3f7ffb2cc3809e5662e58bac63eec6d81049e94
GitHub Action <action@github.com>
2023-01-04 12:35:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/jmartisk/smallryegraphqlclientintellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jmartisk/smallryegraphqlclientintellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jmartisk.smallryegraphqlclientintellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jmartisk.smallryegraphqlclientintellijplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/jmartisk/smallryegraphqlclientintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jmartisk/smallryegraphqlclientintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jmartisk.smallryegraphqlclientintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jmartisk.smallryegraphqlclientintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
